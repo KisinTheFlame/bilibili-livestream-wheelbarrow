60ca199b95177fd4f1a0f6f8dc5d3348ababde83
KisinTheFlame <870853294@qq.com>
2022-02-01 05:53:39 +0800

    Add a panel, not completed.

--- src/Wheelbarrow.ts
+++ src/Wheelbarrow.ts
@@ -4,17 +4,98 @@
     let isRunning: boolean = false;
     let handler: number | null = null;
 
-    function createWheelBarrowButton(
+    function createControlPanel(): HTMLDivElement {
+
+        function createItem(s: string): HTMLDivElement {
+            const item: HTMLDivElement = document.createElement("div");
+            item.style.width = "100%";
+            item.innerText = s;
+            return item;
+        }
+
+        function createItemListContainer(): HTMLDivElement {
+            const itemListContainer: HTMLDivElement = document.createElement("div");
+            itemListContainer.style.width = "100%";
+            itemListContainer.style.maxHeight = "600px";
+            return itemListContainer;
+        }
+
+        function createFunctionBar(): HTMLDivElement {
+            const functionBar: HTMLDivElement = document.createElement("div");
+            functionBar.style.width = "100%";
+
+            const line1: HTMLDivElement = document.createElement("div");
+            line1.style.width = "100%";
+            const addButton: HTMLButtonElement = document.createElement("button");
+            addButton.style.width = "50%";
+            addButton.textContent = "+"
+            const deleteButton: HTMLButtonElement = document.createElement("button");
+            deleteButton.style.width = "50%";
+            deleteButton.textContent = "-";
+            line1.appendChild(addButton);
+            line1.appendChild(deleteButton);
+
+            const line2: HTMLDivElement = document.createElement("div");
+            line2.style.width = "100%";
+            const fileAddButton: HTMLButtonElement = document.createElement("button");
+            fileAddButton.style.width = "33.3%";
+            fileAddButton.textContent = "++";
+            const allDeleteButton: HTMLButtonElement = document.createElement("button");
+            allDeleteButton.style.width = "33.3%";
+            allDeleteButton.textContent = "--";
+            const helpButton: HTMLButtonElement = document.createElement("button");
+            helpButton.style.width = "33.3%";
+            helpButton.textContent = "help";
+            line2.appendChild(fileAddButton);
+            line2.appendChild(allDeleteButton);
+            line2.appendChild(helpButton);
+
+            functionBar.appendChild(line1);
+            functionBar.appendChild(line2);
+            return functionBar;
+        }
+
+        function createControlButton(): HTMLButtonElement {
+            const controlButton: HTMLButtonElement = document.createElement("button");
+            controlButton.style.width = "100%";
+            controlButton.textContent = "Start";
+            return controlButton;
+        }
+
+        const controlPanel: HTMLDivElement = document.createElement("div");
+        // controlPanel.style.display = "none";
+        controlPanel.style.width = "200px";
+        controlPanel.style.position = "absolute";
+        controlPanel.style.bottom = "100%";
+        controlPanel.style.left = "50%";
+        controlPanel.style.transform = "translateX(-50%)";
+        controlPanel.appendChild(createItemListContainer());
+        controlPanel.appendChild(createFunctionBar());
+        controlPanel.appendChild(createControlButton());
+        return controlPanel;
+    }
+
+    function createWheelbarrowButton(
         textArea: HTMLTextAreaElement,
         sendButton: HTMLButtonElement
     ) {
-        const button = document.createElement("button");
-        button.classList.add("icon-item", "icon-font");
+        const button: HTMLButtonElement = document.createElement("button");
         button.textContent = "独轮车";
         button.onclick = () => {
             handleClick(textArea, sendButton)
         };
         return button;
+    }
+
+    function createWheelbarrowComponent(
+        textArea: HTMLTextAreaElement,
+        sendButton: HTMLButtonElement
+    ): HTMLSpanElement {
+        const container: HTMLSpanElement = document.createElement("span");
+        container.classList.add("icon-item", "icon-font");
+        container.appendChild(createWheelbarrowButton(textArea, sendButton));
+        container.appendChild(createControlPanel());
+        return container;
     }
 
     function send(
@@ -42,7 +123,10 @@
         return [textArea, sendButton, toolbar];
     }
 
-    function handleClick(textArea: HTMLTextAreaElement, sendButton: HTMLButtonElement): void {
+    function handleClick(
+        textArea: HTMLTextAreaElement,
+        sendButton: HTMLButtonElement
+    ): void {
         if (!isRunning) {
             const strings: String | null = prompt("输入你要发送的独轮车，各句间用英文逗号分隔。");
             if (strings === null) {
@@ -77,8 +161,7 @@
             toolbar !== null
         ) {
             window.clearInterval(getElementsHandler);
-            // @ts-ignore
-            toolbar.appendChild(createWheelBarrowButton(textArea, sendButton));
+            toolbar.appendChild(createWheelbarrowComponent(textArea, sendButton));
         }
     }, 500);
 })();
